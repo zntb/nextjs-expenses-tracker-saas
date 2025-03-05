540a160449d89e563d15e03c1fd7b048ae33ceac
zntb <zenetibi@yahoo.com>
2025-03-05 11:58:28 +0200

    Add  Home page content

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,26 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div className='text-3xl font-bold'>Home page</div>;
+  return (
+    <div className='bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10'>
+      <Image
+        src='https://bytegrad.com/course-assets/youtube/expensestracker/preview.png'
+        alt='Expenses Tracker app preview'
+        width={700}
+        height={472}
+        className='rounded-md'
+      />
+
+      <div>
+        <h1 className='text-5xl font-semibold my-6 max-w-[500px]'>
+          Track your <span className='font-extrabold'>expenses</span> with ease
+        </h1>
+
+        <p className='text-2xl font-medium max-w-[600px]'>
+          Use Expenses Tracker to easily keep track of your expenses. Get
+          lifetime access for $99.
+        </p>
+      </div>
+    </div>
+  );
 }
